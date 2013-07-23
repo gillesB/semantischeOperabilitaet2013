bc61a45bb145b7077942f7ce1fd8a8a6e7779520
Gilles Baatz <baatzgilles@gmail.com>
2013-07-23 17:44:46 +0200

    #6 bugfixes. Closes #6

--- SI-Hauptprojekt/src/de/htw/queries/Queries.java
+++ SI-Hauptprojekt/src/de/htw/queries/Queries.java
@@ -21,31 +21,54 @@ public class Queries {
 	private static ShortFormProvider shortFormProvider = new SimpleShortFormProvider();
 
 	public static Set<OWLClass> querySport() {
+		//TODO also check if Sportart is also in DB
+		
 		Set<OWLClass> fetchedClasses = ontolgy.doQuery("Sport");
+		String query = "SELECT DISTINCT name FROM  Sportangebot";
+		ResultSet results = null;
+		Set<OWLClass> sportClasses = new HashSet<OWLClass>();
+		try {
+			results = database.createStatement().executeQuery(query);
+			while (results.next()) {
+				String sportName = results
+						.getString("Sportangebot.name");
+				for (OWLClass sportclass : fetchedClasses) {
+					if (shortFormProvider.getShortForm(sportclass).equals(
+							sportName)) {
+						sportClasses.add(sportclass);
+						break;
+					}					
+				}				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return sportClasses;
+	}
+
+	public static Set<OWLClass> queryEinzelsport(Set<OWLClass> inputClasses) {
+		String query = "Einzelsport "
+				+ createAccepableClassesCondition_Ontology(inputClasses);
+		Set<OWLClass> fetchedClasses = ontolgy.doQuery(query);
 
 		return fetchedClasses;
-	} 
-	
-	
-	public static Set<OWLClass> queryEinzelsport(Set<OWLClass> inputClasses) {
-		Set<OWLClass> fetchedClasses = ontolgy.doQuery("Einzelsport");
-
-		return intersection(inputClasses, fetchedClasses);
 	}
 
 	public static Set<OWLClass> queryTeamsport(Set<OWLClass> inputClasses) {
-		Set<OWLClass> fetchedClasses = ontolgy.doQuery("Teamsport");	
+		String query = "Teamsport " + createAccepableClassesCondition_Ontology(inputClasses);
+		Set<OWLClass> fetchedClasses = ontolgy.doQuery(query);
 
-		return intersection(inputClasses, fetchedClasses);
+		return fetchedClasses;
 	}
 
-	public static Set<OWLClass> queryPrice(Set<OWLClass> inputClasses,
-			double d) {
+	public static Set<OWLClass> queryPrice(Set<OWLClass> inputClasses, double d) {
 		String query = "SELECT DISTINCT Sportangebot.name FROM "
 				+ " Sportangebot, Kurs WHERE "
 				+ " Sportangebot.idSportangebot = Kurs.idSportangebot "
-				+ "AND kosten = " + d
-				+ createAccepableClassesCondition(inputClasses);
+				+ "AND kosten <= " + d
+				+ createAccepableClassesCondition_DB(inputClasses);
 
 		ResultSet results = null;
 		try {
@@ -59,54 +82,73 @@ public class Queries {
 	}
 
 	/**
-	 * Iteriert über sportClasses und acceptableClasses und werft alle Sportarten aus sportClasses raus, die sich nicht in 
-	 * acceptableClasses befinden
+	 * Iteriert über sportClasses und acceptableClasses und wirft alle
+	 * Sportarten aus sportClasses raus, die sich nicht in acceptableClasses
+	 * befinden
+	 * 
 	 * @param sportClasses
 	 * @param acceptableClasses
 	 * @return gefiltertes sportClasses
 	 */
 	private static Set<OWLClass> removeUnacceptableClasses(
 			Set<OWLClass> sportClasses, ResultSet acceptableClasses) {
-		if(acceptableClasses == null){
+		if (acceptableClasses == null) {
 			return sportClasses;
 		}
 		
+		Set<OWLClass> acceptableClassesSet = new HashSet<>();
 		try {
 			while (acceptableClasses.next()) {
-			    String sportName = acceptableClasses.getString("Sportangebot.name");
-			    for(OWLClass sportclass : sportClasses){
-			    	if(shortFormProvider.getShortForm(sportclass).equals(sportName)){
-			    		break;
-			    	}
-			    	sportClasses.remove(sportclass);
-			    }			    
+				String sportName = acceptableClasses
+						.getString("Sportangebot.name");
+				for (OWLClass sportclass : sportClasses) {
+					if (shortFormProvider.getShortForm(sportclass).equals(
+							sportName)) {
+						acceptableClassesSet.add(sportclass);
+						break;
+					}
+				}
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		return null;
+
+		return acceptableClassesSet;
 	}
 
-	private static String createAccepableClassesCondition(
+	private static String createAccepableClassesCondition_DB(
 			Set<OWLClass> inputClasses) {
 		StringBuilder condition = new StringBuilder();
+		boolean first = true;
 		for (OWLClass owlClass : inputClasses) {
-			condition.append(" AND '"
-					+ shortFormProvider.getShortForm(owlClass) + "'"
-					+ System.lineSeparator());
+			if(first){
+				condition.append(" AND ");
+				first = false;
+			} else {
+				condition.append(" OR ");
+			}
+			condition.append(" Sportangebot.name = '");
+			condition.append(shortFormProvider.getShortForm(owlClass));
+			condition.append("'"+System.lineSeparator());
 		}
 
 		return condition.toString();
 	}
-	
-	private static Set<OWLClass> intersection(Set<OWLClass> set1, Set<OWLClass> set2){
-		Set<OWLClass> intersection = new HashSet<OWLClass>(set1);
-		intersection.retainAll(set2);
-		return intersection;
+
+	private static String createAccepableClassesCondition_Ontology(
+			Set<OWLClass> inputClasses) {
+		StringBuilder condition = new StringBuilder("and (");
+		int i = 0;
+		for (OWLClass owlClass : inputClasses) {
+			condition.append("" + shortFormProvider.getShortForm(owlClass));
+			if (i < inputClasses.size() - 1) {
+				condition.append(" or ");
+			}
+			i++;
+		}
+		condition.append(")");
+		return condition.toString();
 	}
 
 }
